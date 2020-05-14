30dec6427a868713f1227d719d74152ee91c6687
Dan Anderson <11447979+Donkbonker@users.noreply.github.com>
2020-05-14 18:47:27 +0100

    Revert "broke boucing"
    This reverts commit b6b95e06ae15dbe3144defc7c6919031fe00bb94.

--- ball.js
+++ ball.js
@@ -4,29 +4,42 @@ class Ball {
     this.y = 0;
     this.xv = 0;
     this.yv = 0;
-    this.speed = 2;
+    this.speed = 3;
     this.size = 20;
     this.color = color(random(255), 255, 255);
     this.t = millis();
     this.reset();
+
   }
   move() {
-    let tempx = this.x;
-    let tempy = this.y;
     this.x += this.xv;
     this.y += this.yv;
     if ((this.x < 0) || (this.x > width)) {
+      this.yv += 1;
       this.xv *= -1;
-      this.x = tempx;
     }
     if (this.y < 0) {
       this.yv *= -1;
-      this.y = tempy;
     }
     if (this.y > height) {
       this.reset();
       score -= 10;
     }
+
+    if (millis() - this.t > 500) { //Needed to stop it expliting game.
+      if ((this.y >= y - wid) &&
+        (this.x < x + len) &&
+        (this.x > x - len)) {
+        this.t = millis();
+        score += 50; //Reward for hitting a ball.
+        this.scoreToAdd++;
+        let a = map(this.x - x, -len, len, 0, -PI);
+        this.xv = -cos(a) * this.speed;
+        this.yv = -sin(a) * this.speed;
+        this.yv *= -1;
+      }
+    }
+
   }
 
   show() {
@@ -38,7 +51,8 @@ class Ball {
   reset() {
     this.x = width / 2;
     this.y = height / 2;
-    this.xv = random(-this.speed, this.speed);
-    this.yv = this.speed;
+    let angle = random(0, PI);
+    this.xv = -cos(angle) * this.speed;
+    this.yv = -sin(angle) * this.speed;
   }
 }
